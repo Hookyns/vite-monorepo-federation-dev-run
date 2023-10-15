fbd8a444eb8272e53f8816b3aa4ef8afd8652bb3
Hookyns <hookcze@gmail.com>
2023-10-15 16:45:56 +0200

    Clear, before publish

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# vite-monorepo-federation-dev-run
-Cmd tool for running Vite pnpm monorepo project with module federation
+# vite-federation
+Cmd tool to control pnpm monorepo with Vite and module federation.
+
+To show help use:
+```shell
+vite-federation -h
+```
+
+When you run some command, you can stop it with `Ctrl+C`; all spawned processes will be killed.
+
+Order of projects is determined from the order of patterns in the command then by the order of packages inside pnpm-workspace.yaml file.
+
+## Example
+```shell
+vite-federation serve --host @myapp/host --remotes @myapp-modules/* --libs @myapp-libs/*
+```
+
+This cmd will use package with name `@myapp/host` as the host application, app packages with name matching `@myapp-modules/*` as remotes and packages with name matching `@myapp-libs/*` as libraries.
+Libraries are build first, then remotes are build and started and then the host is started.
+
+You can also specify multiple patterns.
+```shell
+vite-federation serve --host @myapp/host --remotes @myapp-modules/* @myapp-other-modules/* @third-module
+```
+
+## How Does it Work?
+This tool spawn multiple processes so multiple vite commands can be executed.
+
+**Libraries**
+\
+Libraries are executed as `vite build -w`. We expect you to use something like Storybook to develop your libraries, so we just build it; there will be no HMR. If you want HMR, you can treat libs like remotes.
+Tool waits for libraries to be built, then it continues with remotes.
+
+**Remotes**
+\
+Remotes are executed as `vite dev` so you can open only given remote, alone.
+Host does/should not use this served modules. HMR will not work with this. You should rather configure your host to include source code of remotes (`include` in tsconfig), so it will handle the remotes as part of the host. It is the best way how to developer remotes if you want HMR while working from host app.
+
+**Host**
+\
+The host is executed as `vite dev` after libraries and remotes.
--- src/cli-commands/serve-command.ts
+++ src/cli-commands/serve-command.ts
@@ -10,8 +10,6 @@ export async function serveCommand(cli: CLI) {
 	const options = await cli.getCommandLineArguments();
 	const processes: ChildProcess[] = [];
 
-	const tsImport = await import("ts-import");
-
 	// Clear on kill signals
 	process.on("SIGINT", () => clear(processes));
 	process.on("SIGTERM", () => clear(processes));
@@ -26,7 +24,7 @@ export async function serveCommand(cli: CLI) {
 	processes.push(...libs);
 
 	// REMOTES
-	const remotes = await startRemotes(options, tsImport);
+	const remotes = await startRemotes(options);
 	processes.push(...remotes);
 
 	// HOST
@@ -47,52 +45,13 @@ async function startLibs(options: CommandLineArguments): Promise<ChildProcess[]>
 	return Promise.resolve(processes);
 }
 
-async function startRemotes(
-	options: CommandLineArguments,
-	// @ts-ignore
-	tsImport: typeof import("ts-import")
-): Promise<ChildProcess[]> {
+async function startRemotes(options: CommandLineArguments): Promise<ChildProcess[]> {
 	const remotes = selectProjectsInOrder(options.remotes, options.projects);
 	const processes = [];
 
 	for (const remote of remotes) {
 		const process = execCommand("pnpm exec vite dev", remote, magenta);
 		processes.push(process);
-
-		// const process = execCommand("pnpm exec vite build --watch", remote, magenta);
-		// processes.push(process);
-		// await firstBuildDetectionPromise(process);
-
-		// const processPreview = execCommand("pnpm exec vite preview", remote, magenta);
-		// processes.push(processPreview);
-
-		// // console.log("DIR", remote.dir);
-		// // const viteConfigPath = path.resolve(remote.dir, "vite.config.ts");
-		// // console.log("file://" + viteConfigPath.replace("\\", "/"));
-		// // const viteConfig = await tsImport.load("file://" + viteConfigPath.replace("\\", "/"), {});
-		//
-		// // try {
-		// // 	const link = path.resolve(options.hostProject.dir, "src", "@modules", "icobis_module_laboratories");
-		// // 	const target = path.resolve(remote.dir, "dist");
-		// //
-		// // 	fs.symlinkSync(target, link);
-		// // } catch (e) {
-		// // 	console.error(e);
-		// // }
-		//
-		// // const viteConfig = eval(
-		// // 	tsNode
-		// // 		.create({
-		// // 			esm: true,
-		// // 		})
-		// // 		.compile(fs.readFileSync(viteConfigPath, "utf-8"), viteConfigPath)
-		// // );
-		//
-		// // const viteConfig = (await import("file://" + viteConfigPath)).default;
-		// // console.log(viteConfig);
-		//
-		// // const processDev = execCommand("pnpm exec vite preview", remote, magenta);
-		// // processes.push(processDev);
 	}
 
 	return Promise.resolve(processes);
